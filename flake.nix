9c86dd1daff1789445920596a23d0a8f397d3e86
purepani <pani0028@umn.edu>
2023-04-25 20:14:59 +0000

    added jax and gpu support

--- flake.nix
+++ flake.nix
@@ -14,8 +14,16 @@
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {};
-
+      pkgs = import nixpkgs {
+        config.allowUnfree=true;
+        imports = [
+          ./cachix.nix
+        ];
+      };
+      #pkgs_old = import nixpkgs_old {};
+      jaxlib = pkgs.python3Packages.jaxlib.override {
+            cudaSupport=true;
+            };
       packageName = "MagneticHeadTracking";
     in {
       devShell = devenv.lib.mkShell {
@@ -23,7 +31,7 @@
        
         modules = [
           ({pkgs, config, ...}: {
-            packages = [pkgs.zlib pkgs.python3 pkgs.nodejs pkgs.libgccjit pkgs.xorg.libX11 pkgs.libGLU pkgs.libGL pkgs.ffmpeg pkgs.xorg.libXrender pkgs.f2c];
+            packages = [pkgs.zlib pkgs.python3 pkgs.python3Packages.jax jaxlib pkgs.nodejs pkgs.libgccjit pkgs.xorg.libX11 pkgs.libGLU pkgs.libGL pkgs.ffmpeg pkgs.xorg.libXrender pkgs.f2c pkgs.cudatoolkit];
             env.JUPYTER_PATH = "${config.env.DEVENV_STATE}/venv/share/jupyter";
             env.JUPYTER_CONFIG_DIR = "${config.env.DEVENV_STATE}/venv/etc/jupyter";
             languages.python = {
@@ -34,6 +42,12 @@
             languages.typescript = {
               enable = true;
             };
+            enterShell = ''
+              export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH
+              export CUDA_PATH=${pkgs.cudatoolkit}
+              export CUBLAS_LIB=$CUDA_PATH/lib64/libcublas.so
+            '';
+
           })
         ];
       };
