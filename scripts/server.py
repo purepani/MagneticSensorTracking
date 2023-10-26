fd2e3b4924a95efdc926645962e8640ead33649d
Brandon <nguyenbh7@gmail.com>
2023-10-26 20:02:04 +0100

    added server script

---
+++ scripts/server.py
@@ -0,0 +1,12 @@
+from magneticsensortracking import sensors, ui
+import board
+import uvicorn
+
+sensors = sensors.base.SensorGroup([sensors.Sensors.MLX90393(i2c=board.I2C(), address=a) for a in range(0x10, 0x14)], positions=[[0., 0., 0.], [25., 0., 0.], [0., 25., 0.], [25., 25., 0.]])
+
+
+app = ui.main(sensor_group=sensors)
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
+
